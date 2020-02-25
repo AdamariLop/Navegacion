8d1419d09cd0a68662989fcb6f5088e0d452d7c1
adama <adamarilopez04@gmail.com>
2020-02-25 13:01:44 -0600

    Codigo de Pantallas para Navegacion

---
+++ screen/Registrarse.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { Container, Card, Content,  Body, Text, Button, Item, CardItem, Input, Icon} from 'native-base';
+import { StyleSheet, Linking, Alert } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+
+class Registro extends Component {
+  render(){
+    const navegar = this.props.navigation;
+    return (
+      <Container>
+      <Content padder contentContainerStyle={styles.content}>
+        <Card>
+          <CardItem header bordered>
+            <Text style={styles.textCenter}>
+              {this.props.route.params.titulo}
+            </Text>
+          </CardItem>
+          <Item>
+            <Text style={styles.textCenter}>Registrarse con una Red Social o Email</Text>
+          </Item>
+          <Item>
+            <Button primary style={styles.boton_red} onPress={() => Linking.openURL('http://facebook.com')}>
+              <Icon type='AntDesign' name='facebook-square'></Icon></Button>
+            <Button info style={styles.boton_red}><Icon type='FontAwesome' name='twitter-square' onPress={() => Linking.openURL('https://twitter.com/login?lang=es')}></Icon></Button>
+          </Item>
+          <CardItem bordered>
+            <Body>
+              <Text style={styles.textCenter}>o</Text>
+              <Item inlineLabel>
+                <Icon type='FontAwesome' name='user' />
+                  <Input placeholder='Nombre de usuario' />
+                </Item>
+                <Item inlineLabel last>
+                  <Icon type='Entypo' name='email' />
+                  <Input placeholder='Correo' />
+                </Item>
+                <Item inlineLabel last>
+                  <Icon type='FontAwesome' name='lock' />
+                  <Input placeholder='Contraseña' secureTextEntry={true} />
+                </Item>
+            </Body>
+          </CardItem>
+          <CardItem footer bordered>
+            <Button primary style={styles.boton}
+            onPress={()=> Alert.alert('¡Usuario registrado!')}><Text> Registrarse </Text></Button>
+          </CardItem>
+        </Card>
+      </Content>
+    </Container>
+      );
+    }
+}
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center', // CENTRAR CONTENIDO
+  },
+  textCenter: {
+    textAlign: 'center', // ALINEAR EL TEXTO
+    width: '100%' // OCUPAR EL ANCHO DE CARD
+  },
+  boton: {
+    marginLeft: '25%' // AVENTAR DE LADO DERECHO
+  },
+  boton_red: {
+    justifyContent: 'center',
+    width: 165.7
+  }
+});
+
+
+export default Registro;
